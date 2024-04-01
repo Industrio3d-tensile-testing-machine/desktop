2ed2904a42c63873c419c2acb1a77c005006acd7
Martijn-Faber <martijn@fabtronics.nl>
2024-04-01 10:36:27 +0200

    chore: changes

--- src/app.rs
+++ src/app.rs
@@ -1,12 +1,18 @@
-use std::{fmt::{self, Formatter}, time::Duration};
-use serialport;
-use egui::{Ui, Response, WidgetText};
+use std::{fmt::{self, Formatter}, ops::RangeInclusive};
+use egui::{Ui, Response, WidgetText, Align2, Vec2, Color32};
 use strum::IntoEnumIterator;
 use strum_macros::EnumIter;
 use egui_plot::{Line, Plot, PlotPoints};
+use egui_toast::{self, Toasts, Toast, ToastOptions, ToastKind};
 
 mod serial_driver;
+mod icons;
 use self::serial_driver::SerialDriver;
+
+const INDUSTRIO_LOGO: egui::ImageSource<'_> = egui::include_image!("../assets/logo.png");
+const SPECIMEN_DIAGRAM: egui::ImageSource<'_> = egui::include_image!("../assets/specimen.png");
+
+const INDUSTRIO_PRIMARY_COLOR_RGB: Color32 = egui::Color32::from_rgb(204, 0, 2);
 
 #[derive(PartialEq, Debug, Clone, Copy)]
 enum ConnectionState {
@@ -62,6 +68,12 @@ impl Default for UserPreferences {
     }
 }
 
+#[derive(serde::Deserialize, serde::Serialize, Default)]
+pub struct TestParamters {
+    speed: f64,
+    area: f64,
+    max_distance: f64
+}
 
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)]
@@ -72,9 +84,14 @@ pub struct TensileTestingApp {
     connection_state: ConnectionState,
     serial_port: String,
     baud_rate: String,
+    
+    test_parameters: TestParamters,
 
     #[serde(skip)]
     driver : SerialDriver,
+
+    #[serde(skip)]
+    toast: Toasts,
 
     #[serde(skip)]
     data_points : Vec<[f64;2]>,
@@ -87,7 +104,9 @@ impl Default for TensileTestingApp {
             user_preferences: UserPreferences::default(),
             serial_port: Default::default(),
             baud_rate: Default::default(),
+            test_parameters: Default::default(),
             driver : SerialDriver::new(),
+            toast: Toasts::new().anchor(Align2::LEFT_TOP, (10.0, 10.0)).direction(egui::Direction::TopDown),
             data_points : Vec::new(),
         }
     }
@@ -129,21 +148,29 @@ impl TensileTestingApp {
     }
     
     fn panel_ui(&mut self, ui: &mut egui::Ui) {
+        let ports = self.driver.available_ports().expect("No ports available");
 
-
-        let ports = serialport::available_ports().expect("No ports found!");
+        ui.add_space(10.0);
+        ui.vertical_centered_justified(|ui| {
+            ui.add(egui::Image::new(INDUSTRIO_LOGO).max_height(15.0));
+        });
+        ui.add_space(10.0);
 
         egui::CollapsingHeader::new("Connection settings").default_open(true).show(ui, |ui| {
             egui::Frame::group(ui.style()).show(ui, |ui| {
                 egui::Grid::new("state_grid").num_columns(2).spacing([50.0, 8.0]).show(ui, |ui| {
                     let state = ConnectionState::to_string(&self.connection_state);
+
                     ui.label("State:");
                     ui.label(state);
-                    ui.end_row()
-                })
-            });
-    
-            egui::Frame::group(ui.style()).show(ui, |ui| {
+                    ui.end_row();
+                    ui.label("Current position:");
+                    ui.label(format!("{}mm", self.driver.values().position));
+                    ui.end_row();
+                });
+
+                ui.separator();
+
                 ui.add_enabled_ui(!is_serial_connected(&self.connection_state), |ui| {
                 egui::Grid::new("connection_settings_grid")
                     .num_columns(2)
@@ -191,7 +218,8 @@ impl TensileTestingApp {
                         },
                         ConnectionState::Connected => {
                             if full_centered_button(ui, "Disconnect").clicked() {
-                                self.connection_state = ConnectionState::Disconnected
+                                self.connection_state = ConnectionState::Disconnected;
+                                self.driver.close();
                             }
                         },
                         ConnectionState::Disconnected => {
@@ -199,66 +227,102 @@ impl TensileTestingApp {
                                 self.connection_state = ConnectionState::Connecting;
     
                                 let bru32 = self.baud_rate.parse::<u32>().unwrap();
-    
-                                
-                                match serialport::new(&self.serial_port, bru32).timeout(Duration::from_millis(10)).open() {
-                                    Ok(mut serial_interface)  => {
 
-                                        self.driver.set_serial(serial_interface);
-                                        //self.serial_interface = Some(serial_interface);
-                                        // match self.serial_interface.as_deref_mut(){
-                                        //      Some(s) => {
-                                        //         let output = "This is a test. This is only a test.".as_bytes();                                        
-                                        //         s.write(output).expect("Can't write to serial");
-        
-                                        //      },
-                                        //     None => todo!(),
-                                        // }
+                                // self.driver.connect(self.serial_port, bru32);
+                                
+                                // match serialport::new(&self.serial_port, bru32).timeout(Duration::from_millis(10)).open() {
+                                //     Ok(mut serial_interface)  => {
+                                //         self.driver.set_serial(serial_interface);
+                                //         self.connection_state = ConnectionState::Connected;
+                                //     }
+                                //     Err(err) => {
+                                //         self.connection_state = ConnectionState::Disconnected;
+                                //         self.toast.add(Toast { text: err.to_string().into(), kind: ToastKind::Info, options: ToastOptions::default().duration_in_seconds(3.0)});
+                                //     },
+                                // }
+    
+                                match self.driver.open(&self.serial_port, bru32) {
+                                    Ok(_)  => {                    
                                         self.connection_state = ConnectionState::Connected;
                                     }
                                     Err(err) => {
                                         self.connection_state = ConnectionState::Disconnected;
-                                        eprintln!("Failed {err}")
+                                        self.toast.add(Toast { text: err.to_string().into(), kind: ToastKind::Info, options: ToastOptions::default().duration_in_seconds(3.0)});
                                     },
                                 }
-    
                             }
                         },
                     }
             });
         }).fully_open();
 
-        egui::CollapsingHeader::new("Controls").default_open(true).show(ui, |ui| {
-            egui::Frame::group(ui.style()).show(ui, |ui| {
-                ui.vertical(|ui| {
-                ui.add_enabled_ui(!matches!(self.connection_state, ConnectionState::Disconnected), |ui| {
-                    ui.columns(3, |columns| {
-                        columns[0].add(egui::Button::image_and_text(PLAY_ARROW_ICON, "Start").fill(egui::Color32::from_rgb(0, 68, 204)));
-                        columns[1].add(egui::Button::image_and_text(PAUSE_ICON, "Pause"));
-                        columns[2].add(egui::Button::image_and_text(STOP_ICON, "Cancel"))
-                    });
+        self.specimen_settings_panel(ui);
 
-                    ui.add_space(8.0);
-                    ui.horizontal_wrapped(|ui| {
-                        if ui.add(egui::Button::image(BACK_ARROW_ICON)).on_hover_text("Jog back").clicked() {
-                            // actie wanneer de knop wordt ingedrukt
-                            let _ = self.driver.jog(-10);
-                        }
-        
-                        if ui.add(egui::Button::image(HOME_ICON)).on_hover_text("Home").clicked() {
-                            // actie wanneer de knop wordt ingedrukt
-                            let _ = self.driver.start_home();
-                        }
-        
-                        if ui.add(egui::Button::image(FORWARD_ARROW_ICON)).on_hover_text("Jog forward").clicked() {
-                            // actie wanneer de knop wordt ingedrukt
-                            let _ = self.driver.jog(10);
-                        }
-        
+        egui::CollapsingHeader::new("Controls").open(Some(is_serial_connected(&self.connection_state))).show(ui, |ui| {
+            egui::Frame::group(ui.style()).show(ui, |ui| {
+                ui.vertical_centered_justified(|ui| {
+                    ui.add_enabled_ui(!matches!(self.connection_state, ConnectionState::Disconnected), |ui| {
+                        ui.columns(3, |columns| {
+                            columns[0].add(egui::Button::image_and_text(icons::PLAY_ARROW_ICON, "Start").fill(INDUSTRIO_PRIMARY_COLOR_RGB));
+                            columns[1].add(egui::Button::image_and_text(icons::PAUSE_ICON, "Pause"));
+                            columns[2].add(egui::Button::image_and_text(icons::STOP_ICON, "Cancel"))
+                        });
+
+                        ui.add_space(8.0);
+                        ui.add_enabled_ui(!self.driver.is_acknowledge_pending(), |ui| {
+                            ui.horizontal_wrapped(|ui| {
+                                if ui.add_enabled(self.driver.is_homed(), egui::Button::image(icons::BACK_ARROW_ICON)).on_hover_text("Jog back").clicked() {
+                                    // TODO: error
+                                    let _ = self.driver.jog(10).is_err();
+                                }
+                
+                                if ui.add(egui::Button::image(icons::HOME_ICON)).on_hover_text("Home").clicked() {
+                                    // actie wanneer de knop wordt ingedrukt
+                                    let _ = self.driver.start_home();
+                                }
+                
+                                if ui.add_enabled(self.driver.is_homed(), egui::Button::image(icons::FORWARD_ARROW_ICON)).on_hover_text("Jog forward").clicked() {
+                                    // actie wanneer de knop wordt ingedrukt
+                                    let _ = self.driver.jog(-10);
+                                }
+                            });
+                        })
                     });
-                });
                 });
             });
+        });
+    }
+
+    fn specimen_settings_panel(&mut self, ui: &mut Ui) {
+        egui::CollapsingHeader::new("Specimen settings").show(ui, |ui| {
+            egui::Frame::group(ui.style()).show(ui, |ui| {
+                ui.vertical_centered_justified(|ui| {
+                    ui.allocate_ui(Vec2{x: 125.0, y: 80.0}, |ui| {
+                        ui.add(egui::Image::new(SPECIMEN_DIAGRAM));
+
+                        ui.add(egui::DragValue::new(&mut self.test_parameters.area))
+                    });
+
+                    ui.add_space(25.0);
+
+                    egui::Grid::new("specimen_settings_grid")
+                    .num_columns(2)
+                    .spacing([0.0, 8.0])
+                    .show(ui, |ui| {
+                        ui.label("Speed:");
+                        ui.add(egui::DragValue::new(&mut self.test_parameters.speed).suffix("mm/s").clamp_range(RangeInclusive::new(0, 100)));
+                        ui.end_row();
+
+                        ui.label("Max distance:");
+                        ui.add(egui::DragValue::new(&mut self.test_parameters.max_distance).suffix("mm"));
+                        ui.end_row();
+
+                        ui.label("Specimen area:");
+                        ui.add(egui::DragValue::new(&mut self.test_parameters.area).suffix("mm²"));
+                        ui.end_row();
+                    });
+                })
+            })
         });
     }
 
@@ -284,12 +348,43 @@ impl eframe::App for TensileTestingApp {
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, &self);
     }
-
   
     /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Put your widgets into a `SidePanel`, `TopBottomPanel`, `CentralPanel`, `Window` or `Area`.
-        // For inspiration and more examples, go to https://emilk.github.io/egui
+
+        if ctx.input_mut(|i| i.consume_key(egui::Modifiers::NONE, egui::Key::H)) {
+            let _ = self.driver.start_home();
+        }
+
+        if ctx.input_mut(|i| i.consume_key(egui::Modifiers::NONE, egui::Key::ArrowLeft)) {
+            let result = self.driver.jog(10);
+
+            match result {
+                Ok(_) => {},
+                Err(err) => {
+                    self.toast.add(Toast {
+                        text: err.to_string().into(),
+                        kind: ToastKind::Info,
+                        options: ToastOptions::default().duration_in_seconds(1.0)
+                    });
+                },
+            };
+        }
+
+        if ctx.input_mut(|i| i.consume_key(egui::Modifiers::NONE, egui::Key::ArrowRight)) {
+            let result = self.driver.jog(-10);
+
+            match result {
+                Ok(_) => {},
+                Err(err) => {
+                        self.toast.add(Toast {
+                        text: err.to_string().into(),
+                        kind: ToastKind::Info,
+                        options: ToastOptions::default().duration_in_seconds(1.0)
+                    });
+                },
+            };
+        }
 
         self.update_data();
 
@@ -315,27 +410,22 @@ impl eframe::App for TensileTestingApp {
         });
 
         egui::SidePanel::new(egui::panel::Side::Right, "side_panel").show(ctx, |ui| {
-            self.panel_ui(ui)
+            self.panel_ui(ui);
+            ui.vertical_centered_justified(|ui| {
+                ui.small("v1.0.0 - ©Industrio");
+            })
         });
 
         // request new repaint
         ctx.request_repaint();
+        self.toast.show(ctx);
+    }
 
+    fn on_exit(&mut self, _gl: Option<&eframe::glow::Context>) {
+        self.driver.close();
     }
 }
 
-
-
-
-// trait CustomUi {
-//     fn full_centered_button(ui: &mut Ui, text: impl Into<WidgetText>) -> Response;
-// }
-
-// impl CustomUi for egui::Ui {
-//     fn full_centered_button(ui: &mut Ui, text: impl Into<WidgetText>) -> Response {
-//         ui.add_sized(egui::vec2(ui.available_width(), 0.0),egui::Button::new(text))
-//     }
-// }
 
 fn full_centered_button(ui: &mut Ui, text: impl Into<WidgetText>) -> Response {
     full_width(ui, egui::Button::new(text))
--- src/app/serial_driver.rs
+++ src/app/serial_driver.rs
@@ -1,17 +1,19 @@
-use std::io::BufRead;
+use std::{io::BufRead, time::Duration};
 use anyhow::ensure;
+use serialport::{self, SerialPort, Error, SerialPortInfo};
 use log::{info, debug, error};
 
 #[derive(Debug, Default, Copy, Clone)]
 pub struct Values {
   pub sample : u32,
-  pub position : i32,
+  pub position : f32,
   pub tensile : i32,
 }
 
 pub struct SerialDriver {
   values : Values,
-  wait_for_response : bool,
+  acknowledge_pending : bool,
+  is_homed: bool,
   serial_interface: Option<Box<dyn serialport::SerialPort>>,
 }
 
@@ -19,31 +21,57 @@ impl SerialDriver {
   pub fn new() -> Self {
     Self {
       values : Values::default(),
-      serial_interface : None,
-      wait_for_response : false,
+      acknowledge_pending: false,
+      is_homed: false,
+      serial_interface: None,
     }
   }
 
-  pub fn set_serial(&mut self, serial_interface : Box<dyn serialport::SerialPort>) {
-    self.serial_interface = Some( serial_interface);
+  pub fn open(&mut self, serial_port: &String, baud_rate: u32) -> Result<(), Error> {
+    let result = serialport::new(serial_port, baud_rate).timeout(Duration::from_millis(10)).open();
+
+    self.serial_interface = Some(result?);
+
+    Ok(())
   }
 
+  pub fn close(&mut self) {
+    drop(self.serial_interface.take());
+    self.acknowledge_pending = false;
+    self.is_homed = false;
+  }
+
+  pub fn available_ports(&mut self) -> anyhow::Result<Vec<SerialPortInfo>> {
+    match serialport::available_ports() {
+        Ok(res) => anyhow::Ok(res),
+        Err(err) => anyhow::bail!(err.to_string()),
+    }
+  }
 
   pub fn start_home(&mut self) -> anyhow::Result<()> {
-    ensure!(!self.wait_for_response, "Homing in progress");
-    self.wait_for_response = true;
-    self.send_message("G28\r\n")
+    ensure!(!self.acknowledge_pending, "Homing in progress");
+    self.acknowledge_pending = true;
+    let res = self.send_message("G28\r\n");
+    self.is_homed = true;
+    res
   }
 
-  pub fn jog(&mut self, delta: i32 ) -> anyhow::Result<()>  {
-    let new_pos = i32::max( self.values.position + delta, 0 );
-    let msg = format!("G0 X{new_pos}\r\n" );
+  pub fn jog(&mut self, delta: i32) -> anyhow::Result<()>  {
+    ensure!(!self.acknowledge_pending, "Jog in progress");
+    ensure!(self.is_homed, "Tensile tester not homed, please home first");
+
+    let pos = i32::max( self.values.position as i32 + delta, 0 );
+    // debug!("new pos: {pos}");
+
+    let msg = format!("G0 X{pos}\r\n" );
     self.send_message(&msg)
   }
-  
+
+  pub fn babystep(&mut self) -> anyhow::Result<()> {
+    todo!();
+  }
 
   pub fn update(&mut self) -> Option<Values> {
-    
     if let Some(s) = self.serial_interface.as_deref_mut() {
 
       let mut reader = std::io::BufReader::new(s);
@@ -55,10 +83,14 @@ impl SerialDriver {
     
           match res {
             Ok(n) => {
-              debug!("RECV:{n}>{}", &line);
+              // debug!("RECV:{n}>{}", &line);
               let mut line_parts = line.split_whitespace();
     
                 if let Some(tag) = line_parts.next() {
+                  if tag.contains("ok") {
+                    self.acknowledge_pending = false;
+                  }
+
                   if tag.contains("X:") {
                     let val = &tag[2..];
                     self.values.position = val.parse().unwrap();
@@ -88,13 +120,12 @@ impl SerialDriver {
     }
   }
 
-
   pub fn send_message(&mut self, msg : &str) -> anyhow::Result<()> {
 
     info!("SENDING MESSAGE:{}:", msg);
     let bts = msg.as_bytes();
 
-    match self.serial_interface.as_deref_mut(){
+    match self.serial_interface.as_deref_mut() {
       Some(s) => {
         let bytes_written = s.write(bts)?;
         debug!("Wrote: {bytes_written} bytes");
@@ -103,5 +134,17 @@ impl SerialDriver {
       },
       None => anyhow::bail!("No serial interface"),
     }
+  }
+
+  pub fn is_acknowledge_pending(&self) -> bool {
+    self.acknowledge_pending
+  }
+
+  pub fn is_homed(&self) -> bool {
+    self.is_homed
+  }
+
+  pub fn values(&self) -> Values {
+    self.values
   }
 }
